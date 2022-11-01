ede2264342d3786d083421cd8fc63fdd710f448c
sadogilles <sadowizzy@gmail.com>
2022-11-01 09:09:31 +0100

    subscription to employee service to get data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from './employee.service';
+import { IEmployee } from './iemployee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'employee-manager-app';
+  
+  public employeeList:IEmployee[]=[];
+
+  constructor(private employeeService:EmployeeService){}
+
+  ngOnInit(){
+    this.getEmployees(); // call the api on initialisation
+  }
+
+  private getEmployees():void{
+    this.employeeService.getEmployees().subscribe(
+    (response:IEmployee[])=>{
+      this.employeeList = response;
+    },
+    (error:HttpErrorResponse)=>{
+      alert(error.message);
+    }
+    );
+  }
 }
